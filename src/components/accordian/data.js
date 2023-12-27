7e508f17328e17d4a75f5c918104beb5c00d9afa
samuelbahiru <samuelbahiru93@gmail.com>
2023-12-27 15:19:02 +0300

    inital commit

---
+++ src/components/accordian/data.js
@@ -0,0 +1,29 @@
+const data = [
+  {
+    id: 1,
+    question: "What is the capital of France?",
+    answer: "Paris",
+  },
+  {
+    id: 2,
+    question: "Who wrote 'Romeo and Juliet'?",
+    answer: "William Shakespeare",
+  },
+  {
+    id: 3,
+    question: "What is the largest planet in our solar system?",
+    answer: "Jupiter",
+  },
+  {
+    id: 4,
+    question: "In which year did the Titanic sink?",
+    answer: "1912",
+  },
+  {
+    id: 5,
+    question: "What is the main ingredient in guacamole?",
+    answer: "Avocado",
+  },
+];
+
+export default data;
